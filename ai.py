406b3ceb045670ad815731c259f1201cecc37b0f
joshbeyerr <117863098+joshbeyerr@users.noreply.github.com>
2023-10-29 13:37:21 -0400

    Update ai.py

--- ai.py
+++ ai.py
@@ -1,8 +1,9 @@
 import requests
 
+# ignore, just a fun ai file
 
 def ask(content):
-    api_key = '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'
+    api_key = ''
     api_url = 'https://api.openai.com/v1/chat/completions'
     s = requests.session()
 
@@ -32,21 +33,3 @@ ask(inpt)
 
 
 
-
-
-#fake
-# openai.api_key = api_key
-#
-# # Define a prompt
-# prompt = "Translate the following English text to French: 'Hello, how are you?'"
-#
-# # Make an API call
-# response = openai.Completion.create(
-#     engine="davinci",  # You can choose a different engine if needed
-#     prompt=prompt,
-#     max_tokens=50  # Adjust as needed
-# )
-
-# Extract and print the AI's response
-# ai_response = response.choices[0].text
-# print(ai_response)
